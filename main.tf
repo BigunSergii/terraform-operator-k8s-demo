bff4252e8a51c4a8d31fd064b0cb5eaf3554e9a7
SerhiiBihun <41513347+SerhiiBihun@users.noreply.github.com>
2021-06-07 15:05:35 +0300

    Update main.tf
    add random_pet

--- main.tf
+++ main.tf
@@ -1,17 +1,29 @@
-provider "aws" {
-  region = "us-east-1"
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_instance" "myubuntu" {
+#   ami = "ami-09e67e426f25ce0d7"
+#   instance_type = "t3.micro"
+#   count = "2"
+
+#   tags = {
+#     Name = "My-Ubuntu-Server"
+#     Owner = "Srhii Bihun"
+#     Edition = "First change"
+#     Killed = "true"
+#     Test = "true"
+#   } 
+# }
+
+resource "random_pet" "console_password" {
+    length = 4
 }
 
-resource "aws_instance" "myubuntu" {
-  ami = "ami-09e67e426f25ce0d7"
-  instance_type = "t3.micro"
-  count = "2"
+output "lol" {
+    value = "${random_pet.console_password.id}"
+}
 
-  tags = {
-    Name = "My-Ubuntu-Server"
-    Owner = "Srhii Bihun"
-    Edition = "First change"
-    Killed = "true"
-    Test = "true"
-  } 
+output "rofl" {
+    value = "${random_pet.console_password.id}"
 }
