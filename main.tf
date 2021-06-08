ad8eed1807e6123277f7b5d8b8be34c02be8b9a7
Serhii Bihun <sbihun@cisco.com>
2021-06-08 14:03:55 +0300

    chaged lenght

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
+#  provider "aws" {
+#    region = "us-east-1"
+#  }
 
-# resource "aws_instance" "myubuntu" {
-#   ami = "ami-09e67e426f25ce0d7"
-#   instance_type = "t3.micro"
-#   count = "2"
+#  resource "aws_instance" "myubuntu" {
+#    ami = "ami-09e67e426f25ce0d7"
+#    instance_type = "t3.micro"
+#    count = "2"
 
-#   tags = {
-#     Name = "My-Ubuntu-Server"
-#     Owner = "Srhii Bihun"
-#     Edition = "First change"
-#     Killed = "true"
-#     Test = "true"
-#   } 
-# }
+#    tags = {
+#      Name = "My-Ubuntu-Server"
+#      Owner = "Srhii Bihun"
+#      Edition = "First change"
+#      Killed = "true"
+#      Test = "true"
+#    } 
+#  }
 
 resource "random_pet" "console_password" {
-    length = 4
+    length = 8
 }
 
 output "lol" {
